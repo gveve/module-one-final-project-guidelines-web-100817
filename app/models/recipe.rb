f7e7fb9dd59785c0e007fc175b6c7957aaaa54ac
github username <github email address>
2017-10-24 13:17:19 -0400

    added get_ingredient_names method

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ActiveRecord::Base
 
+  def self.get_ingredients(drink_id)
+    Recipe.where("drink_id = #{drink_id}").map do |recipe|
+      Ingredient.find_by(id: recipe.ingredient_id).ingredient_name
+    end
+  end
 end
