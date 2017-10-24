418885b9508cf284bca2ec1579dbda00f1849854
github username <github email address>
2017-10-24 17:22:57 -0400

    added formatting to output statments

--- app/command_line_interface.rb
+++ app/command_line_interface.rb
@@ -1,16 +1,13 @@
 def welcome
   # puts out a welcome message here!
-  puts "For centuries, humans have looked to the heavens for guidance. \nWe, at Drunk Astrology,
-    feel that we can use Astrology as a powerful and tool for \nunderstanding ourselves, others, and what we should drink. In a few simple questions
-     we can provide you with guidance on your drinking habits for the day."
+  puts "\nFor centuries, humans have looked to the heavens for guidance.\nWe, at \e[1mDrunk Astrology CLI©\e[0m, feel that we can use Astrology as a tool for understanding ourselves, and what we should drink.\n\nIn a few simple questions we can provide you with guidance in the form of a Drunkoscope©."
 end
 
 def get_user_sign
-  puts "Please enter your Sun Sign:
-        Your Sun Sign, which is the zodiacal sign position of the Sun at
-        the time of your birth. Each Sun Sign has a different perspective on the
-        aspects of each day. Our exclusive Daily Drunkoscope offers an overview of
-        how the planets think you should get wasted today.
+  puts "\nYour Sun Sign, which is the zodiacal sign position of the Sun at the time of your birth. Each Sun Sign has a different perspective on the aspects of each day. Our exclusive Daily Drunkoscope offers an overview of how the planets think you should get wasted today.
+
+        Please enter your Sun Sign:
+
         Aries - March 21 - April 20
         Taurus - April 21 - May 21
         Gemini - May 22 - June 21
@@ -22,23 +19,26 @@ def get_user_sign
         Sagittarius - November 23 - December 22
         Capricorn - December 23 - January 20
         Aquarius - January 21 - February 19
-        Pisces - February 20- March 20"
+        Pisces - February 20- March 20\n\n"
   sun_sign = gets.chomp.downcase
   if ['aries', 'taurus', 'gemini', 'cancer', 'leo', 'virgo', 'libra', 'scorpio', 'sagittarius', 'capricorn', 'aquarius', 'pisces'].include?(sun_sign)
     else
-      puts "Invalid input. Please try again."
+      puts "\n\n-----------------------------------"
+      puts "\n\e[1mInvalid input. Please try again.\e[0m"
       get_user_sign
   end
   sun_sign
 end
 
 def get_mood_return_drink(user)
-  puts "Are you happy or sad? Enter 1 for happy, 2 for sad."
+  puts "\n-----------------------------------"
+  puts "\nAre you happy or sad? Enter 1 for happy, 2 for sad."
   # use gets to capture the user's input. This method should return that input, downcased.
   mood = gets.chomp.downcase
   if mood == 1 || 2
     else
-      puts "Invalid input. Please try again."
+      puts "\n\n-----------------------------------"
+      puts "\n\e[1mInvalid input. Please try again.\e[0m"
       get_mood_from_user
   end
   # binding.pry
@@ -46,6 +46,7 @@ def get_mood_return_drink(user)
 end
 
 def get_user_name
-  puts "What is your name?"
+  puts "\n-----------------------------------"
+  puts "\nWhat is your name?"
   gets.chomp.downcase
 end
