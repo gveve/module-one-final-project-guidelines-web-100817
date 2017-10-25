a9e8a09f6e7f73d2bf4da1182d4686aa56919749
github username <github email address>
2017-10-25 16:30:18 -0400

    e just formatting and adding waaaaaaaay too much ascii art

--- app/models/interesting_methods.rb
+++ app/models/interesting_methods.rb
@@ -1,20 +1,29 @@
-
+def sign_checker(sign)
+  if %w[scorpio aquarius].include?(sign)
+    "scorpio/aquarius"
+  elsif %w[leo sagittarius].include?(sign)
+    "leo/sagittarius"
+  else sign
+  end
+  # binding.pry
+end
 
 def find_drinks_based_on_sign(star_sign)
-  #Find a drink based on your star sign
-  Drink.where(sign: star_sign)
+  # Find a drink based on your star sign
+  Drink.where(sign: sign_checker(star_sign), alcoholic: "Alcoholic")
+  # binding.pry
 end
 
 def find_drink_based_on_mood(mood, star_sign)
-    #Find a random drink based on your start sign and mood
-    #Mood - 1 = Happy || 2 = Sad
-    #uses find_drinks_based_on_sign as helper method
+  # Find a random drink based on your start sign and mood
+  # Mood - 1 = Happy || 2 = Sad
+  # uses find_drinks_based_on_sign as helper method
   drinks = find_drinks_based_on_sign(star_sign)
-  if mood == "1" #happy = 3 or less ingredients in drink
-    return drinks.select {|drink| drink.no_ingredients <= 3}.sample
-    #samples to get random drink
-  elsif mood == "2" #sad = 4 or more ingredients in drink
-    return drinks.select{|drink| drink.no_ingredients >= 4}.sample
-    #samples to get random drink
+  if mood == '1' # happy = 3 or less ingredients in drink
+    return drinks.select { |drink| drink.no_ingredients <= 3 }.sample
+    # samples to get random drink
+  elsif mood == '2' # sad = 4 or more ingredients in drink
+    return drinks.select { |drink| drink.no_ingredients >= 4 }.sample
+    # samples to get random drink
   end
 end
--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,5 @@
 require_relative '../config/environment'
 
-# create_drinks #Seeds database
-
 def run_program
   welcome
   user_name = get_user_name
@@ -11,13 +9,21 @@ def run_program
     user.save
   else
     user = User.find_by(username: user_name)
+    if user.sign == nil
+      user.sign = get_user_sign
+      user.save
+    end
   end
   current_drink = get_mood_return_drink(user)
   # binding.pry
   drunkoscope = Horoscope.create(drink_id: current_drink.id, user_id: user.id)
   drunk = drunkoscope.horoscope_generator
   # binding.pry
-  puts drunk
+  sleep(0.5)
+    puts star_reader
+  sleep(2)
+    puts drunk
+  sleep(4)
   user_input = get_user_selection
   while user_input != "3"
     if user_input == "1"
@@ -25,10 +31,14 @@ def run_program
       # binding.pry
       drunkoscope = Horoscope.create(drink_id: current_drink.id, user_id: user.id)
       drunk = drunkoscope.horoscope_generator
-      puts drunk
+      sleep(0.5)
+        puts star_reader
+      sleep(2)
+        puts drunk
     elsif user_input == "2"
       Horoscope.get_last_3_horoscopes(user.id).each {|horoscope| puts horoscope.horoscope}
     end
+    sleep(4)
     user_input = get_user_selection
   end
 end
